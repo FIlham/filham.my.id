25fa25147adb766886cfd236d4a135c6fcc4b6e9
filham <fahmiilham281207@gmail.com>
2024-06-05 21:10:57 +0700

    update portfolio

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ function Projects() {
         <section id="projects">
             <h1 className="m-0 md:text-center">Karya Saya</h1>
             <h4 className="mt-2 mb-4 md:mb-8 md:text-center">Karya apa yang telah saya buat?</h4>
-            <p className="font-medium w-[28rem] md:text-center md:mx-auto">Untuk karya/project biasanya saya upload di Instagram atau Github untuk sekedar publishing/promosi atau hanya sekedar untuk menyimpan nya saja.</p>
+            <p className="font-medium max-w-[28rem] md:text-center md:mx-auto">Untuk karya/project biasanya saya upload di Instagram atau Github untuk sekedar publishing/promosi atau hanya sekedar untuk menyimpan nya saja.</p>
             <div className="card bg-base-content text-base-100">
                 <div className="card-body">
                     <h1 className="card-title mb-0 text-base-100">Tracking Anime</h1>
